8d88cef0ad6cd54c46c75f160f9c4df9866aab78
Thoms Stuart <31195539+ThomasStuart@users.noreply.github.com>
2020-04-05 11:31:00 -0700

    Create mcode.py

---
+++ mcode.py
@@ -0,0 +1,69 @@
+
+class Node: 
+    def __init__(self, n, es , pos , vis):
+        self.name = n
+        self.edges = es   # because its a list
+        self.position = pos
+        self.visited = vis
+        
+    def getName(self):
+        return self.name
+    
+    def getEdges(self):
+        return self.edges 
+    
+    def getPosition(self):
+        return self.position 
+    
+    def getVisited(self):
+        return self.visited 
+
+# write it from scratch
+class Edge: 
+    def __init__(self, n1,n2):
+        self.startNode = n1
+        self.endNode   = n2
+        
+    # getter startNode
+    def getStartNode(self):
+        return startNode
+    
+    # getter endNode
+    def getStartNode(self):
+        return endNode
+
+class Graph:
+    
+    def __init__(self):
+        self.node_dict = None
+        self.edge_dict = None
+        
+    def simple_graph(self):
+        
+        #Exercise:  create the 4 nodes (A,S,B,X):
+        nodeS = Node('S', [],   (0,0), False)
+        nodeA = Node('A', [], (-10,0), False)
+        nodeB = Node('B', [],  (10,0), False)
+        nodeX = Node('X', [],  (0,10), False)
+        
+    
+        # create the edges 
+        e1 = Edge(nodeS, nodeX)
+        e2 = Edge(nodeA, nodeS)
+        e3 = Edge(nodeS, nodeB)
+        
+        
+        #add the individual edges to their respected nodes
+        nodeS.edges.append( e1 )
+        nodeS.edges.append( e2 )
+        nodeS.edges.append( e3 )
+        
+        #jason for node a
+        nodeA.edges.append( e2 )
+        
+        # richardson for node b 
+        nodeB.edges.append( e3 )
+        
+        #node X
+        nodeX.edges.append( e1 )
+        
